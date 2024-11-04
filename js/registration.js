e2c3e0da7279cfad4dc7390c170e3e2b47eba210
SLY <IT_HEAD@ITs-MacBook-Pro.local>
2024-11-04 13:51:54 +0000

    First push midsem

---
+++ js/registration.js
@@ -0,0 +1,5 @@
+let submit = document.getElementById(submitButton);
+submit.addEventListener("click", myfuction)
+function myfunction(){
+  alert('It has been clicked!')
+}
---
+++ js/yh.js
@@ -0,0 +1,7 @@
+function add(total){
+  let num1 = 3;
+  let num2 = 49;
+  total = num1 + num2;
+  return total
+}
+console.log(add())
